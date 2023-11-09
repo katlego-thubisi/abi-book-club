097bd99b5b873c556f465fae84e062af2cd711cc
Masego Dipela <masegolxve@gmail.com>
2023-11-09 12:01:17 +0200

    U/I Bug fixes

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -21,7 +21,10 @@ async function Page() {
                 Complete your profile now to use Abi's Book Club
             </p>
             <section className="mt-9 bg-dark-2 p-10">
-                <AccountProfile/>
+                <AccountProfile 
+                    user={userData}
+                    btnTitle="Continue"
+                />
             </section>
         </main>
     ) 
