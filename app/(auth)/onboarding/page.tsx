3dcaf010dc91797f17ae19950c6d7fbcf53d4b82
Masego Dipela <masegolxve@gmail.com>
2023-11-09 11:14:22 +0200

    Created forms folder in components folder. began building out the logic that will take form data form the onboarding process

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,4 +1,19 @@
+import AccountProfile from "@/components/forms/AccountProfile";
+import { currentUser } from "@clerk/nextjs";
+
 async function Page() {
+    const user = await currentUser();
+
+    const userInfo = {};
+    
+    const userData = {
+        id: user?.id,
+        ObjectId: userInfo?._id,
+        username: userInfo?.username || user?.username,
+        name: userInfo?.name || user?.firstName || "",
+        bio: userInfo?.bio || "",
+        image: userInfo?.image || user?.imageUrl,
+    }
     return (
         <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
             <h1 className="head-text">Onboarding</h1>
@@ -6,7 +21,7 @@ async function Page() {
                 Complete your profile now to use Abi's Book Club
             </p>
             <section className="mt-9 bg-dark-2 p-10">
-
+                <AccountProfile/>
             </section>
         </main>
     ) 
