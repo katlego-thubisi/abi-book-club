3d59ec4c5e4d82e1f51a1496b3c98ca45692ed33
Masego Dipela <masegolxve@gmail.com>
2023-11-12 16:24:00 +0200

    Completed onboarding section

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -17,7 +17,9 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { UserValidation } from '@/lib/validations/user';
 import * as z from "zod"
 import Image from 'next/image';
-import { ChangeEvent } from 'react';
+import { ChangeEvent, useState } from 'react';
+import { isBase64Image } from '@/lib/utils';
+import { useUploadThing } from "@/lib/uploadthing";
 
 interface Props {
     user:{
@@ -31,25 +33,53 @@ interface Props {
     btnTitle: string;
 }
 
-const AccountProfile = ( { user, btnTitle}: Props ) => {
-    const form = useForm({
-        resolver: zodResolver(UserValidation),
-        defaultValues: {
-            profile_photo: user?.image || "",
-            name: user?.name || "",
-            username: user?.username || "",
-            bio: user?.bio|| "",
-        }
-    })
-    const handleImage = (e: ChangeEvent, fieldChange: (value: string) => void ) =>{
+const AccountProfile = ({ user, btnTitle }: Props) => {
+  const { startUpload } = useUploadThing("media");
+  const [files, setFiles] = useState<File[]>([]);
+  const form = useForm<z.infer<typeof UserValidation>>({
+    resolver: zodResolver(UserValidation),
+    defaultValues: {
+      profile_photo: user?.image ? user.image : "",
+      name: user?.name ? user.name : "",
+      username: user?.username ? user.username : "",
+      bio: user?.bio ? user.bio : "",
+    },
+  });
+    const handleImage = (
+      e: ChangeEvent<HTMLInputElement>,
+      fieldChange: (value: string) => void
+    ) => {
       e.preventDefault();
-
-    }
-    function onSubmit(values: z.infer<typeof UserValidation>) {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
-        console.log(values)
+  
+      const fileReader = new FileReader();
+  
+      if (e.target.files && e.target.files.length > 0) {
+        const file = e.target.files[0];
+        setFiles(Array.from(e.target.files));
+  
+        if (!file.type.includes("image")) return;
+  
+        fileReader.onload = async (event) => {
+          const imageDataUrl = event.target?.result?.toString() || "";
+          fieldChange(imageDataUrl);
+        };
+  
+        fileReader.readAsDataURL(file);
       }
+    };
+    
+  const onSubmit = async (values: z.infer<typeof UserValidation>) => {
+    const blob = values.profile_photo;
+
+    const hasImageChanged = isBase64Image(blob);
+    if (hasImageChanged) {
+      const imgRes = await startUpload(files);
+
+      if (imgRes && imgRes[0].fileUrl) {
+        values.profile_photo = imgRes[0].fileUrl;
+      }
+    }
+  }
 
     return (
         <Form {...form}>
