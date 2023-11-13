38a90960bc18f20e13f19342ce05807c338dc9bb
Katlego Thubisi <katlego.thubisi@goodcode.co.za>
2023-11-13 18:23:34 +0200

    Inital commit for Entries tab on profile

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -4,6 +4,11 @@ import { fetchUser } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { profileTabs } from "@/constants";
+import Image from "next/image";
+import EntriesTab from "@/components/shared/EntriesTab";
+
 async function Page({ params }: { params: { id: string } }) {
   const user = await currentUser();
   if (!user) return null;
@@ -23,6 +28,45 @@ async function Page({ params }: { params: { id: string } }) {
         imgUrl={userInfo.image}
         bio={userInfo.bio}
       />
+
+      <div className="mt-9">
+        <Tabs defaultValue="journal" className="w-full">
+          <TabsList className="tab">
+            {profileTabs.map((tab: any) => (
+              <TabsTrigger key={tab.label} value={tab.value} className="tab">
+                <Image
+                  src={tab.icon}
+                  alt={tab.label}
+                  width={24}
+                  height={24}
+                  className="object-contain"
+                />
+
+                <p className="max-sm:hidden">{tab.label}</p>
+
+                {tab.label === "Entries" && (
+                  <p className="ml-1 rounded-sm bg-light-4 px-2 py-1 !text-tiny-medium text-light-2">
+                    {userInfo?.threads?.length}
+                  </p>
+                )}
+              </TabsTrigger>
+            ))}
+          </TabsList>
+          {profileTabs.map((tab: any) => (
+            <TabsContent
+              key={`content-${tab.label}`}
+              value={tab.value}
+              className="w-full text-light-1"
+            >
+              <EntriesTab
+                currentUserId={user.id}
+                accountId={userInfo.id}
+                accountType="User"
+              />
+            </TabsContent>
+          ))}
+        </Tabs>
+      </div>
     </section>
   );
 }
---
+++ components/shared/EntriesTab.tsx
@@ -0,0 +1,37 @@
+import { fetchUserPosts } from "@/lib/actions/user.actions";
+import React from "react";
+import { redirect } from "next/navigation";
+import EntryCard from "../cards/EntryCard";
+
+interface Props {
+  currentUserId: string;
+  accountId: string;
+  accountType: string;
+}
+
+const EntriesTab = async ({ currentUserId, accountId, accountType }: Props) => {
+  //TODO: Fetch profile threads
+  let result = await fetchUserPosts(accountId);
+
+  if (!result) redirect("/");
+
+  return (
+    <section className="mt-9 flex flex-col gap-10">
+      {result.threads.map((entry: any) => {
+        <EntryCard
+          key={entry._id}
+          id={entry._id}
+          currentUserId={currentUserId}
+          parentId={entry.parentId}
+          content={entry.text}
+          author={entry.author} //TODO: Update
+          community={entry.community} //TODO: Update
+          createdAt={entry.createdAt}
+          comments={entry.children}
+        />;
+      })}
+    </section>
+  );
+};
+
+export default EntriesTab;
