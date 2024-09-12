5166cc441ec92deacd9deb9ed692626cac49be90
Katlego Thubisi <katlego.thubisi@goodcode.co.za>
2024-09-12 20:05:03 +0200

    Update home page feed

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,58 +1,25 @@
 import CommunityCard from "@/components/cards/CommunityCard";
+import Communities from "@/components/sections/communities";
+import Entries from "@/components/sections/entries";
 
 import { fetchCommunities } from "@/lib/actions/community.actions";
 
 async function Page() {
   // Fetch communities
-  const result = await fetchCommunities({
-    searchString: "",
-    pageNumber: 1,
-    pageSize: 25,
-  });
+  // const result = await fetchCommunities({
+  //   searchString: "",
+  //   pageNumber: 1,
+  //   pageSize: 4,
+  // });
 
-  const activeCommunities = result.communities
-    ? result.communities.filter((c) => c?.status === "active")
-    : [];
+  // const activeCommunities = result.communities
+  //   ? result.communities.filter((c) => c?.status === "active")
+  //   : [];
 
   return (
     <section>
-      <div className="flex justify-between align-middle">
-        <div className="flex-col">
-          <h1 className="head-text text-black dark:text-white leading-none">
-            Find Your Book Club
-          </h1>
-          <h1 className="head-text cursive">Community</h1>
-          <img
-            src="\assets\underline.png"
-            alt="Text Underline"
-            width="150px"
-            height="10px"
-            className="underline"
-          ></img>
-          <p className="mt-7 text-black dark:text-white">
-            Hop into a book club and access curated content by your favorite
-            creators. Heck, you could even create your own!
-          </p>
-        </div>
-        <div className="flex items-center"></div>
-      </div>
-      {/* Search Bar*/}
-
-      {/* Dialog for community form */}
-
-      <div className="mt-9 flex flex-wrap gap-4">
-        {activeCommunities.length === 0 ? (
-          <p className="no-result">No Clubs</p>
-        ) : (
-          <>
-            {activeCommunities.map((community) => (
-              <CommunityCard
-                community={JSON.parse(JSON.stringify(community))}
-              />
-            ))}
-          </>
-        )}
-      </div>
+      <Entries />
+      <Communities />
     </section>
   );
 }
---
+++ components/sections/entries.tsx
@@ -0,0 +1,41 @@
+import { fetchPosts } from "@/lib/actions/journal.actions";
+import React from "react";
+import EntryCard from "../cards/EntryCard";
+
+const Entries = async () => {
+  const result = await fetchPosts(1, 30);
+
+  return (
+    <section className="mt-9 flex flex-col gap-10">
+      {result.posts.map((post: any, index) => (
+        <EntryCard
+          key={post._id}
+          id={post._id}
+          currentUserId={""}
+          content={post.text}
+          author={{
+            name: post.author.name,
+            image: post.author.image,
+            id: post.author.id,
+            _id: post.author._id,
+          }}
+          community={
+            post.community
+              ? {
+                  _id: post.community._id,
+                  id: post.community.id,
+                  name: post.community.name,
+                  image: post.community.image,
+                }
+              : null
+          }
+          createdAt={post.createdAt}
+          comments={JSON.parse(JSON.stringify(post.children))}
+          likes={JSON.parse(JSON.stringify(post.likes))}
+        />
+      ))}
+    </section>
+  );
+};
+
+export default Entries;
